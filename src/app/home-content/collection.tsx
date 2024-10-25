43498dd28f8739d6f8e9789b770a413791978650
Ahmad-Hafizh <ahaaafizh@gmail.com>
2024-10-25 09:27:26 +0700

    modifying the web

--- src/app/home-content/collection.tsx
+++ src/app/home-content/collection.tsx
@@ -4,11 +4,12 @@ export default function Collection() {
   return (
     <div className="collection">
       <h2 className="subtitle">all collection</h2>
-      <div className="carousel-collection">
-        <CollectionCard id="porsche" />
-        <CollectionCard id="brabus" />
-        <CollectionCard id="singer" />
-        {/* <div className="card" id="porsche">
+      <div className="carousel-wrapper">
+        <div className="carousel-collection">
+          <CollectionCard id="porsche" />
+          <CollectionCard id="brabus" />
+          <CollectionCard id="singer" />
+          {/* <div className="card" id="porsche">
           <p>porsche</p>
         </div>
         <div className="card" id="brabus">
@@ -17,6 +18,7 @@ export default function Collection() {
         <div className="card" id="singer">
           <p>singer</p>
         </div> */}
+        </div>
       </div>
     </div>
   );
